83d794e0c3895deea26b014327577e9896dd5841
Umberto <umbertopellegrino96@gmail.com>
2020-07-21 15:21:18 +0200

    aggiunta numero e classe bg

--- js/jscript.js
+++ js/jscript.js
@@ -1,4 +1,42 @@
 function randomNumInside() {
+  var randomNum = $("#randomNum");
+  var cell = 0;
+
+  randomNum.click(function functionName() {
+
+    if (cell <=35) {
+      $.ajax({
+
+        url:'https://flynn.boolean.careers/exercises/api/random/int',
+        metod:'GET',
+        success:function (data,state) {
+          var success = data['succes'];
+          var number = data['response'];
+
+          if (number <= 5) {
+            var posizione = "#" + cell;
+            $(posizione).append(number);
+            $(posizione).addClass("yellow")
+            cell++;
+          } else if (number >= 5){
+            var posizione = "#" + cell;
+            $(posizione).append(number);
+            $(posizione).addClass("green")
+            cell++;
+          }
+
+
+        },
+        error :function (request,state,error) {
+          console.log("error");
+        }
+      });
+    }else {
+      alert("le celle sono piene!");
+    }
+
+  })
+
 }
 
 
